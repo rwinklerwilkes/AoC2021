a1c9308e94a7dd95c2e846d0758bf918193a7e2e
rwinklerwilkes <rich.w.sas@gmail.com>
2021-12-27 15:25:25 -0500

    Day 24, messed up one of the calculations.

--- day24.py
+++ day24.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 
 input_data = get_data(year=2021, day=24)
 
+#Lots of help from https://www.reddit.com/r/adventofcode/comments/rnejv5/comment/hps5hgw/?utm_source=share&utm_medium=web2x&context=3
+
 test_data = """inp w
 add z w
 mod z 2
@@ -59,7 +61,12 @@ class ALU:
 
     def eql(self, a, b):
         b_type, b = self.int_or_var(b)
-        self.storage[a] = (self.storage[a] == b) * 1
+
+        if self.storage[a]==b:
+            store_val = 1
+        else:
+            store_val = 0
+        self.storage[a] = store_val
 
     def parse_instruction(self, instruction):
         match instruction.split(' '):
@@ -88,16 +95,25 @@ class ALU:
 
         return program
 
+    def run_program_2(self, instructions, input):
+        self.reset()
+        all_instructions = instructions.split('\n')
+        for i, inst in enumerate(all_instructions):
+            pi = self.parse_instruction(inst)
+            if inst.split(' ')[0] == 'inp':
+                pass
+            pi(input)
+
     def run_program(self, program, input):
         self.reset()
         for inst in program:
             inst(input)
 
 def part_one(input_data):
-    input_test = [int(i) for i in '45989929969199']
+    input_test = [int(i) for i in '45989929946199']
     alu = ALU()
     program = alu.parse_program(input_data)
-    alu.run_program(program, input_test)
+    alu.run_program_2(input_data, input_test)
     result = alu.storage['z']
     if result == 0:
         # valid
@@ -107,6 +123,17 @@ def part_one(input_data):
         print(f'Invalid, {result}')
 
 def part_two(input_data):
-    pass
+    input_test = [int(i) for i in '11912814611156']
+    alu = ALU()
+    program = alu.parse_program(input_data)
+    alu.run_program_2(input_data, input_test)
+    result = alu.storage['z']
+    if result == 0:
+        # valid
+        print('Valid')
+    elif result != 0:
+        # invalid
+        print(f'Invalid, {result}')
 
 part_one(input_data)
+part_two(input_data)
